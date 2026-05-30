8989d16926763bbd8229843d00a9986fa734b30a
Katherin <gr24017@ues.edu.sv>
2026-05-29 20:58:24 -0600

    cambios de coneccion

--- Mini_Proyecto_Cine/Conexion.cs
+++ Mini_Proyecto_Cine/Conexion.cs
@@ -6,7 +6,7 @@ namespace Mini_Proyecto_Cine
     {
         public static MySqlConnection ObtenerConexion()
         {
-            string cadena = "Server=localhost;Database=cine_db;Uid=root;Pwd=;";
+            string cadena = "server=mysql-proyectosu.alwaysdata.net;port=3306;database=proyectosu_cine;uid=proyectosu_engel;password=3ng3lb3r7;";
             return new MySqlConnection(cadena);
         }
     }
